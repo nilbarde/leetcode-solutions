0632aaa1adb23d8a32b5051b453a669858afc421
nilbarde <nihalbarde@gmail.com>
2024-09-06 18:05:25 +0000

    root functions

--- leetcode.py
+++ leetcode.py
@@ -1,8 +1,12 @@
+import time
+
 def evaluate(function, inputs, outputs):
     for input_num, (i, o) in enumerate(zip(inputs, outputs)):
+        a = time.time()
         function_output = function(*i)
+        b = time.time()
         if o == function_output:
-            print(f"input {input_num} solved correctly")
+            print(f"input {input_num} solved correctly in {b-a}ms")
         else:
             raise Exception(f"Output not matched \n\
     input: {i}\n\
---
+++ sample.py
@@ -0,0 +1,18 @@
+# link -> https://leetcode.com/problems/sample/
+
+from leetcode import evaluate
+
+class Solution:
+    def function(self, x: int) -> int:
+        return x
+
+
+if __name__ == "__main__":
+    inputs = [
+        (123, ),
+    ]
+    outputs = [
+        (123),
+    ]
+    x = Solution()
+    evaluate(x.function, inputs, outputs)
