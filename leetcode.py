bf71b5eef0dd9bf2d5436d6711f5b680abc2917d
nilbarde <nihalbarde@gmail.com>
2024-09-08 13:24:43 +0000

    time logging

--- leetcode.py
+++ leetcode.py
@@ -1,12 +1,16 @@
 import time
 
-def evaluate(function, inputs, outputs):
+def evaluate(function, inputs, outputs, evaluate_time=False):
     for input_num, (i, o) in enumerate(zip(inputs, outputs)):
         a = time.time()
-        function_output = function(*i)
+        if evaluate_time:
+            for _ in range(100):
+                function_output = function(*i)
+        else:
+            function_output = function(*i)
         b = time.time()
         if o == function_output:
-            print(f"input {input_num} solved correctly in {b-a}ms")
+            print(f"input {input_num} solved correctly in {b-a}sec")
         else:
             raise Exception(f"Output not matched \n\
     input: {i}\n\
